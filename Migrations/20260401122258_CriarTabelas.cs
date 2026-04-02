32a0a5ba7998da56f0524d6a6c1db115cd63400f
victtorH <estevamvicttor@gmail.com>
2026-04-02 15:41:21 -0300

    Created_Home

---
+++ Migrations/20260401122258_CriarTabelas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuloMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
